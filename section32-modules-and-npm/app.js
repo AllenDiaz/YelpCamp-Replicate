05a693f329f41b932d67b6ec1b4e29cf1f2953f3
AllenD <allengdiaz2019@gmail.com>
2025-11-22 16:12:40 +0800

    clean main for the project and created branch for the lesson i learned from bootcamp

--- section32-modules-and-npm/app.js
+++
@@ -1,7 +0,0 @@
-const math = require('./math')
-const cats = require('./shelter/index')
-
-
-// console.log(math);
-console.log("Require all of the directories", cats)
-
--- section32-modules-and-npm/jokester/index.js
+++
@@ -1,8 +0,0 @@
-const jokes = require("give-me-a-joke")
-const colors = require("colors");
-const cowsays = require('cowsay')
-
-console.dir(jokes)
-jokes.getRandomDadJoke(function (joke) {
-    console.log(joke.rainbow);
-})
